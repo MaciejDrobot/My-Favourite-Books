fa89cbd6a03aa70284c538200ab494b3aaf4448e
Maciej Drobot <maciejdrobot@gmail.com>
2020-04-30 16:38:20 +0100

    sql service added

---
+++ database-SQL-service/src/main/java/dev/mdrobot/databaseSQLservice/DatabaseSqlServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.mdrobot.databaseSQLservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSqlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseSqlServiceApplication.class, args);
+	}
+
+}
---
+++ database-SQL-service/src/test/java/dev/mdrobot/databaseSQLservice/DatabaseSqlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mdrobot.databaseSQLservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseSqlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
